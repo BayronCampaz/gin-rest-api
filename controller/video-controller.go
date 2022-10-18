03cb7efc5b78ce640f2d71badbcbf6c910b23588
Bayron Campaz <bayron@thinkbridge.com>
2022-10-18 15:23:41 -0500

    Changes before deploy

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -1,21 +1,30 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/BayronCampaz/gin-rest-api/entity"
 	"github.com/BayronCampaz/gin-rest-api/service"
+	"github.com/BayronCampaz/gin-rest-api/validators"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type VideoController interface {
 	FindAll() []entity.Video
-	Save(ctx *gin.Context) entity.Video
+	Save(ctx *gin.Context) error
+	ShowAll(ctx *gin.Context)
 }
 
 type videoController struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(s service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
 	return &videoController{
 		service: s,
 	}
@@ -25,9 +34,25 @@ func (vc *videoController) FindAll() []entity.Video {
 	return vc.service.FindAll()
 }
 
-func (vc *videoController) Save(ctx *gin.Context) entity.Video {
+func (vc *videoController) Save(ctx *gin.Context) error {
 	var video entity.Video
-	ctx.BindJSON(&video)
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
+	if err != nil {
+		return err
+	}
 	vc.service.Save(video)
-	return video
+	return nil
+}
+
+func (vc *videoController) ShowAll(ctx *gin.Context) {
+	videos := vc.service.FindAll()
+	data := gin.H{
+		"title":  "Video Page",
+		"videos": videos,
+	}
+	ctx.HTML(http.StatusOK, "index.html", data)
 }
--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/BayronCampaz/gin-rest-api/controller"
+	"github.com/BayronCampaz/gin-rest-api/middlewares"
 	"github.com/BayronCampaz/gin-rest-api/service"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -14,15 +18,46 @@ var (
 )
 
 func main() {
-	server := gin.Default()
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, videoController.FindAll())
-	})
+	setupLogOutput()
 
-	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, videoController.Save(ctx))
-	})
+	server := gin.New()
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	server.Use(gin.Recovery(),
+		middlewares.Logger(),
+		//middlewares.BasicAuth(),
+		gindump.Dump())
+
+	apiRoutes := server.Group("/api")
+
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid"})
+			}
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
 
 	server.Run(":8000")
+}
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }
